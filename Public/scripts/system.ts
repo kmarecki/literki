8bb845c5553c1b4d1b164a303461e0cb7a4995b2
Krzysztof M <marecki.krzysztof@gmail.com>
2015-07-20 00:21:38 +0200

    Fix date formatting

--- Public/scripts/system.ts
+++ Public/scripts/system.ts
@@ -1,12 +1,12 @@
 ﻿import moment = require('moment');
 
-    export function urlParam(name: string): string {
-        var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
-        return results != null  ? results[1] : null;
-    }
+export function urlParam(name: string): string {
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    return results != null  ? results[1] : null;
+}
 
-    export function formatSeconds(seconds: number, format: string): string {
-            var hours = Math.floor(seconds / 60);
-            var minutes = seconds % 60;
-            return moment(hours + "" + minutes, "mmss").format("mm:ss");
-        }
+export function formatSeconds(seconds: number, format: string): string {
+    var minutes = Math.floor(seconds / 60);
+    var rest = seconds % 60;
+    return moment({ minutes: minutes, second: rest }).format(format);
+}
