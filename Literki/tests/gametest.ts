b22f299e629cc350f2317c4819b26df895d38f4e
Krzysztof Marecki <marecki.krzysztof@gmail.com>
2015-08-19 23:20:36 +0200

    Add mocha test

---
+++ Literki/tests/gametest.ts
@@ -0,0 +1,22 @@
+﻿/// <reference path="../typings/mocha/mocha.d.ts" />
+
+import assert = require('assert');
+
+describe("Game Test Suite", () => {
+    before(() => {
+        console.log("Before method");
+    });
+
+    after(() => {
+        console.log("After method");
+    });
+
+    it("Test A", () => {
+        assert.ok(true, "This shouldn't fail");
+    });
+
+    it("Test B", () => {
+        assert.ok(1 === 1, "This shouldn't fail");
+        assert.ok(false, "This should fail ts");
+    });
+});
